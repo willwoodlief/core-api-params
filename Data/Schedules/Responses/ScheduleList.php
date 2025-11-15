e40c2f3e8ce883b56a32207cd92888eb2b97589e
Will Woodlief <willwoodlief@hexbatch.com>
2025-11-15 03:50:12 -0600

    Allow schedule list to be empty

--- Data/Schedules/Responses/ScheduleList.php
+++ Data/Schedules/Responses/ScheduleList.php
@@ -10,21 +10,23 @@ use Illuminate\Support\Collection;
 use OpenApi\Attributes as OA;
 use Spatie\LaravelData\Data;
 
+use Spatie\LaravelData\Optional;
 use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 
 #[TypeScript]
 #[OA\Schema(schema: 'ScheduleList')]
 class ScheduleList extends Data implements IResponse
 {
+    /**
+     * @param Collection<Schedule>|Optional $data
+     */
     public function __construct(
 
         #[OA\Property( title: 'Schedules',description: "list of schedules",type: 'array',items:  new OA\Items(type: Schedule::class))]
-        public Collection $data,
+        public Collection|Optional $data,
 
         #[OA\Property( title: 'Meta')]
-        public CursoratedMetaData $meta,
-
-
+        public CursoratedMetaData $meta
 
     ) {
     }
